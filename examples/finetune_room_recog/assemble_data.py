1237b8c9bbde2e815ba25690e2ea516d29d7073b
Jesse Vander Does <jesse@htmlfusion.com>
2015-06-06 12:43:28 -0700

    Added script for generating data.txt and test.txt files based on directory listings

---
+++ examples/finetune_room_recog/assemble_data.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""
+List images from a directory and write
+Caffe ImagesDataLayer training file.
+"""
+import os
+import argparse
+import multiprocessing
+import itertools
+
+example_dirname = os.path.abspath(os.path.dirname(__file__))
+caffe_dirname = os.path.abspath(os.path.join(example_dirname, '../..'))
+training_dirname = os.path.join(caffe_dirname, 'data/room_recog')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='List images and output training output.')
+    parser.add_argument(
+        '-i', '--images', nargs='+',
+        help="Images sorted grouped by directory")
+    parser.add_argument(
+        '-t', '--test', nargs='+',
+        help="Images sorted grouped by for use in test set")
+
+    args = parser.parse_args()
+    rooms = itertools.groupby(args.images, lambda i: os.path.basename(os.path.dirname(i)))
+    test = itertools.groupby(args.test, lambda i: os.path.basename(os.path.dirname(i)))
+
+    if not os.path.exists(training_dirname):
+        os.makedirs(training_dirname)
+        
+    with open(os.path.join(training_dirname, 'train.txt'), 'w') as f:
+        for idx, room in enumerate(rooms):
+            for image_file in room[1]:
+                if image_file not in args.test:
+                    f.write('%s %s\n' % (image_file, idx))
+
+
+    with open(os.path.join(training_dirname, 'test.txt'), 'w') as f:
+        for idx, room in enumerate(test):
+            for image_file in room[1]:
+                if image_file:
+                    f.write('%s %s\n' % (image_file, idx))
+
+
